22f5b041f5587def344761c7aa5b0fbcb54a615b
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-13 15:36:07 -0500

    Movimiento de las barras definido. Falta animación

--- main.js
+++ main.js
@@ -58,28 +58,62 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10; //Velocidad de movimiento de las barras
     }
     //Para agregarle movimiento más adelante.
     self.Bar.prototype = {
         down: function(){
-
+            this.y += this.speed;
         },
         up: function(){
-
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return "X: " + this.x + " Y: " + this.y;
         }
     }
 })();
+
+//Declaración de los objetos con alcance global
+var board = new Board(800, 400);
+var barRight = new Bar(750, 100, 20, 100, board);
+var barLeft = new Bar(20, 100, 20, 100, board);
+var canvas = document.getElementById("canvas");
+var board_view = new BoardView(canvas, board);
+
+//Listener que captura las teclas presionadas.
+document.addEventListener("keydown", function(ev){
+    switch(ev.keyCode){
+        case 32: //Cada que se presione espacio, el juego se pausará o se reanudará
+            ev.preventDefault();
+            board.playing = !board.playing;
+            break;
+        case 38: //Si presiona flecha hacia arriba, la barra derecha se mueve hacia arriba
+            ev.preventDefault();
+            barRight.up();
+            break;
+        case 40: //Si presiona flecha hacia abajo, la barra derecha se mueve hacia abajo
+            ev.preventDefault()
+            barRight.down();
+            break;
+        case 83: //Si presiona tecla S, la barra izquierda se mueve hacia abajo
+            ev.preventDefault();
+            barLeft.down();
+            break;
+        case 87: //Si presiona la tecla W, la barra izquierda se mueve hacia arriba
+            ev.preventDefault();
+            barLeft.up();
+            break;
+    }
+});
+
 
 //Una vez se cargue la ventana, llama al método main
 window.addEventListener("load", main);
 
 //Función desde la cual se desplegará todo lo necesario para ejecutar el juego.
 function main(){
-    var board = new Board(800, 400);
-    var barRight = new Bar(750, 100, 20, 100, board);
-    var barLeft = new Bar(20, 100, 20, 100, board);
-    var canvas = document.getElementById("canvas");
-    var board_view = new BoardView(canvas, board);
+    
     board_view.draw();
     
 }
