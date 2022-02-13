27e25b4b7a2022a52c3b459b6d42cee7e61ab998
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-13 16:38:43 -0500

    Animación de movimiento de barras completo. Clase Ball definida. La pelota aparece en pantalla sin animación

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
         get elements(){
             //Se asigna como copia para no sobrecargar la memoria
             var elements = this.bars.map(function(el){ return el; });
-            elements.push(this.ball);
+            elements.push(this.ball); //Ya definida la clase Ball, podemos quitar el comentario
             return elements;
         }
     }
@@ -30,24 +30,51 @@
     }
 
     self.BoardView.prototype = {
+        //Cada acción limpia la animación anterior y dibuja la actual
+        clean: function(){
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+        },
         //Itera la lista de elementos y los dibuja a cada uno
         draw: function(){
             for (var i = this.board.elements.length - 1; i >= 0; i--){
                 var el = this.board.elements[i];
                 draw(this.ctx, el);
             }
+        },
+        play: function(){
+            this.clean(); //Cada llamado a controller, limpia la pantalla
+            this.draw(); //Cada llamado a controller, dibuja en pantalla
         }
     }
     function draw(ctx, element){
-        if (element != null && element.hasOwnProperty("kind")){ //Evita errores en etapa temprana
-            switch(element.kind){
-                case "rectangle":
-                    ctx.fillRect(element.x, element.y, element.width, element.height);
-                    break;
-            }
+        switch(element.kind){ //Según el tipo de elemento que llegue, dibujará uno u otro.
+            case "rectangle":
+                ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
 })();
+
+(function(){
+    self.Ball = function(x, y, radius, board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.board = board;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.kind = "circle";
+
+        board.ball = this;
+    }
+})();
+
 //Declaración de la clase Bar
 (function(){
     self.Bar = function(x, y, width, height, board){
@@ -80,9 +107,12 @@ var barRight = new Bar(750, 100, 20, 100, board);
 var barLeft = new Bar(20, 100, 20, 100, board);
 var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(400, 200, 10, board);
+
 
 //Listener que captura las teclas presionadas.
 document.addEventListener("keydown", function(ev){
+    ev.preventDefault();
     switch(ev.keyCode){
         case 32: //Cada que se presione espacio, el juego se pausará o se reanudará
             ev.preventDefault();
@@ -107,13 +137,11 @@ document.addEventListener("keydown", function(ev){
     }
 });
 
-
-//Una vez se cargue la ventana, llama al método main
-window.addEventListener("load", main);
+//Para que se refresque la pantalla a la velocidad de cada dispositivo
+window.requestAnimationFrame(controller);
 
 //Función desde la cual se desplegará todo lo necesario para ejecutar el juego.
-function main(){
-    
-    board_view.draw();
-    
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller); //Inicia un bucle de refresqueo
 }
